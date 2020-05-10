0fc756cb271ceb449ea5b2b480f579509f0bfc73
YUKIKO IKEDA <applelele@gmail.com>
2020-05-10 18:09:00 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,12 +5,6 @@ module Players
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       computer_move = valid_moves.sample
       # computer_move = (1..9).to_a.sample #==[1, 2, 3, 4, 5, 6, 7, 8, 9].sample
-      # if board.valid_move?(computer_move)
-      #   binding.pry
-      #   board.update(computer_move, self)
-      # else
-      #   move(board)
-      # end
     end
 
     # def move(board)
